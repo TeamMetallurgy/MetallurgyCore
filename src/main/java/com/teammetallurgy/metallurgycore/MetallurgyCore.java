40a0bb598d69af21c76e81212c3a6a7ab01d45bb
Glassmaker <JeanGlassmaker@mail.com>
2014-07-17 02:28:12 +0300

    Bump to forge 1180 and version number bump

--- src/main/java/com/teammetallurgy/metallurgycore/MetallurgyCore.java
+++ src/main/java/com/teammetallurgy/metallurgycore/MetallurgyCore.java
@@ -7,11 +7,12 @@ import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(name = MetallurgyCore.MODNAME, modid = MetallurgyCore.MODID)
+@Mod(name = MetallurgyCore.MODNAME, modid = MetallurgyCore.MODID, version = MetallurgyCore.VERSION)
 public class MetallurgyCore
 {
     public static final String MODID = "MetallurgyCore";
     public static final String MODNAME = "Metallurgy Core";
+    public static final String VERSION = "4.0.3";
 
     @Instance(MetallurgyCore.MODID)
     public static MetallurgyCore instance;
