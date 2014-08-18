827fcec08283db86bd39f18a393affe8140c770e
Glassmaker <JeanGlassmaker@mail.com>
2014-08-18 22:35:48 +0300

    Added ItemStack Support to creative tabs

--- src/main/java/com/teammetallurgy/metallurgycore/CreativeTab.java
+++ src/main/java/com/teammetallurgy/metallurgycore/CreativeTab.java
@@ -1,38 +1,60 @@
 package com.teammetallurgy.metallurgycore;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 public class CreativeTab extends CreativeTabs
 {
     private Item item = Items.apple;
+    private ItemStack itemstack;
 
-	public CreativeTab(String label)
+    public CreativeTab(String label)
     {
         super(label);
     }
-	
-	public CreativeTab(String label, Item item)
-	{
-		this(label);
-		setItem(item);
-	}
+
+    public CreativeTab(String label, Item item)
+    {
+        this(label);
+        setItem(item);
+    }
 
     @Override
+    @SideOnly(Side.CLIENT)
+    public ItemStack getIconItemStack()
+    {
+        if (this.itemstack == null)
+        {
+            this.itemstack = new ItemStack(this.getTabIconItem(), 1, this.func_151243_f());
+        }
+
+        return this.itemstack;
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
     public Item getTabIconItem()
     {
-        return this.item ;
+        return this.item;
     }
 
     public void setItem(Item item)
     {
-		this.item = item;
-	}
+        this.item = item;
+    }
 
-	public void setItem(Block block)
-	{
-		setItem(Item.getItemFromBlock(block));
-	}
+    public void setItem(Block block)
+    {
+        setItem(Item.getItemFromBlock(block));
+    }
+
+    public void setItemStack(ItemStack itemStack)
+    {
+        this.itemstack = itemStack;
+    }
 }
