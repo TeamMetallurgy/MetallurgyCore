b74182b8af4b7f4555f1eb1a1c936b6a34d22c7a
Glassmaker <JeanGlassmaker@mail.com>
2014-10-03 11:47:33 +0300

    Version bump to 4.0.4

--- src/main/java/com/teammetallurgy/metallurgycore/MetallurgyCore.java
+++ src/main/java/com/teammetallurgy/metallurgycore/MetallurgyCore.java
@@ -12,7 +12,7 @@ public class MetallurgyCore
 {
     public static final String MODID = "MetallurgyCore";
     public static final String MODNAME = "Metallurgy Core";
-    public static final String VERSION = "4.0.3";
+    public static final String VERSION = "4.0.4";
 
     @Instance(MetallurgyCore.MODID)
     public static MetallurgyCore instance;
